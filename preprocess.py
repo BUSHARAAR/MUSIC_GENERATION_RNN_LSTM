e0854b6a732fdf7655b428d28a2de142e6d6c993
BUSHARAAR <73915595+BUSHARAAR@users.noreply.github.com>
2021-11-22 13:27:16 +0530

    Add files via upload

--- preprocess.py
+++ preprocess.py
@@ -16,10 +16,14 @@ Created on Thu Nov 18 11:59:45 2021
 #load folk songs
 
 import os
+import json
 import music21 as m21
 
-
-KERN_DATASET_PATH = "C:/Users/HP/Documents/datascience/AI MUSIC/generating melody with RNN/deutschl/test"
+KERN_DATASET_PATH = "deutschl/test"
+SAVE_DIR = "dataset"
+SINGLE_FILE_DATASET = "C:/Users/HP/Documents/datascience/AIMUSIC/generating_melody/file_dataset"
+MAPPING_PATH = "mapping.json"
+SEQUENCE_LENGTH = 64
 
 # durations are expressed in quarter length
 ACCEPTABLE_DURATIONS = [
@@ -32,6 +36,7 @@ ACCEPTABLE_DURATIONS = [
     3,
     4 # whole note
 ]
+
 
 def load_songs_in_kern(dataset_path):
     """Loads all kern pieces in dataset using music21.
@@ -89,6 +94,44 @@ def transpose(song):
     return tranposed_song
 
 
+def encode_song(song, time_step=0.25):
+    """Converts a score into a time-series-like music representation. Each item in the encoded list represents 'min_duration'
+    quarter lengths. The symbols used at each step are: integers for MIDI notes, 'r' for representing a rest, and '_'
+    for representing notes/rests that are carried over into a new time step. Here's a sample encoding:
+        ["r", "_", "60", "_", "_", "_", "72" "_"]
+    :param song (m21 stream): Piece to encode
+    :param time_step (float): Duration of each time step in quarter length
+    :return:
+    """
+
+    encoded_song = []
+
+    for event in song.flat.notesAndRests:
+
+        # handle notes
+        if isinstance(event, m21.note.Note):
+            symbol = event.pitch.midi # 60
+        # handle rests
+        elif isinstance(event, m21.note.Rest):
+            symbol = "r"
+
+        # convert the note/rest into time series notation
+        steps = int(event.duration.quarterLength / time_step)
+        for step in range(steps):
+
+            # if it's the first time we see a note/rest, let's encode it. Otherwise, it means we're carrying the same
+            # symbol in a new time step
+            if step == 0:
+                encoded_song.append(symbol)
+            else:
+                encoded_song.append("_")
+
+    # cast encoded song to str
+    encoded_song = " ".join(map(str, encoded_song))
+
+    return encoded_song
+
+
 def preprocess(dataset_path):
 
     # load folk songs
@@ -96,7 +139,7 @@ def preprocess(dataset_path):
     songs = load_songs_in_kern(dataset_path)
     print(f"Loaded {len(songs)} songs.")
 
-    for song in songs:
+    for i, song in enumerate(songs):
 
         # filter out songs that have non-acceptable durations
         if not has_acceptable_durations(song, ACCEPTABLE_DURATIONS):
@@ -106,25 +149,74 @@ def preprocess(dataset_path):
         song = transpose(song)
 
         # encode songs with music time series representation
+        encoded_song = encode_song(song)
 
         # save songs to text file
+        save_path = os.path.join(SAVE_DIR, str(i))
+        with open(save_path, "w") as fp:
+            fp.write(encoded_song)
+
+
+def load(file_path):
+    with open(file_path, "r") as fp:
+        song = fp.read()
+    return song
+
+
+def create_single_file_dataset(dataset_path, file_dataset_path, sequence_length):
+    """Generates a file collating all the encoded songs and adding new piece delimiters.
+    :param dataset_path (str): Path to folder containing the encoded songs
+    :param file_dataset_path (str): Path to file for saving songs in single file
+    :param sequence_length (int): # of time steps to be considered for training
+    :return songs (str): String containing all songs in dataset + delimiters
+    """
+
+    new_song_delimiter = "/ " * sequence_length
+    songs = ""
+
+    # load encoded songs and add delimiters
+    for path, _, files in os.walk(dataset_path):
+        for file in files:
+            file_path = os.path.join(path, file)
+            song = load(file_path)
+            songs = songs + song + " " + new_song_delimiter
+
+    # remove empty space from last character of string
+    songs = songs[:-1]
+
+    # save string that contains all the dataset
+    with open(file_dataset_path, "w") as fp:
+        fp.write(songs)
+
+    return songs
+
+
+def create_mapping(songs, mapping_path):
+    """Creates a json file that maps the symbols in the song dataset onto integers
+    :param songs (str): String with all songs
+    :param mapping_path (str): Path where to save mapping
+    :return:
+    """
+    mappings = {}
+
+    # identify the vocabulary
+    songs = songs.split()
+    vocabulary = list(set(songs))
+
+    # create mappings
+    for i, symbol in enumerate(vocabulary):
+        mappings[symbol] = i
+
+    # save voabulary to a json file
+    with open(mapping_path, "w") as fp:
+        json.dump(mappings, fp, indent=4)
+
+
+def main():
+    preprocess(KERN_DATASET_PATH)
+    songs = create_single_file_dataset(SAVE_DIR, SINGLE_FILE_DATASET, SEQUENCE_LENGTH)
+    create_mapping(songs, MAPPING_PATH)
 
 
 if __name__ == "__main__":
-
-    # load songs
-    songs = load_songs_in_kern(KERN_DATASET_PATH)
-    print(f"Loaded {len(songs)} songs.")
-    song = songs[0]
-
-    print(f"Has acceptable duration? {has_acceptable_durations(song, ACCEPTABLE_DURATIONS)}")
-
-    # transpose song
-    transposed_song = transpose(song)
-    #song.show()
-  #  transposed_song.show()
-  
-  
-m = m21.converter.parse("C:/Users/HP/Documents/datascience/AI MUSIC/generating melody with RNN/Aja")
-m.show()
-
+    main()
